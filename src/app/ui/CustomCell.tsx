fe295152624c71891937773e8830940ccae973f4
shumilika <shumilika@gmail.com>
2024-09-01 19:15:43 +0200

    cell background changes & added timer hook

--- src/app/ui/CustomCell.tsx
+++ src/app/ui/CustomCell.tsx
@@ -1,30 +1,26 @@
 import { Col } from 'antd';
 import React, { useState } from 'react';
 import CustomInput from './CustomInput';
-import style from './gameBoard.module.css'
 
 interface CustomCellProps {
     cellValue: string; 
     correctValue:string;
+    style:string;
 }
 
-const CustomCell:React.FC<CustomCellProps> = ({cellValue, correctValue}) => {
+const CustomCell:React.FC<CustomCellProps> = ({cellValue, correctValue,style}) => {
 
   const [value, setValue] = useState('')
-  const [background, setBackground] = useState('inherit')
 
-  
 
     return (
         <Col span={2.67} >
                 <div 
-                  style={{background:background}}
-                 className={style.gridStyle}>
+                 className={style}>
                   {cellValue === '.' ? <CustomInput 
                   correctvalue={correctValue}
                   value={value}
                   onChange={setValue}
-                  onChangeBackground={setBackground}
                   /> : cellValue}
                 </div>
               </Col>
