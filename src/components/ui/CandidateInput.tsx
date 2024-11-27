9ccfff56b8318b970d8288d988b8027f94823b2f
shumilika <shumilika@gmail.com>
2024-11-27 17:25:04 +0100

    candidate numbers check

---
+++ src/components/ui/CandidateInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from 'react';
+import style from '../../styles/candidateInput.module.css'
+
+
+
+interface CandidateNumbersProps {
+    selectedValues: string[];
+    onSelect: (value: string) => void;
+  }
+  
+
+const CandidateInput:React.FC<CandidateNumbersProps> = ({selectedValues,onSelect}) => {
+
+  const plainOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const [hoveredValue, setHoveredValue] = useState<string | null>(null);
+
+  const handleMouseEnter = (value: string) => {
+    setHoveredValue(value);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredValue(null);
+  };
+
+ 
+ return (
+    <div className={style.candidateBox}>
+      {plainOptions.map((option) => (
+        <span
+          key={option}
+          className={`${style.number} ${
+            hoveredValue === option || selectedValues.includes(option)
+              ? style.visible
+              : ""
+          }`}
+          onMouseEnter={() => handleMouseEnter(option)}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => onSelect(option)}
+        >
+          {option}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+export default CandidateInput;
--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -5,6 +5,7 @@ import style from '../../styles/customInput.module.css'
 import { useDispatch } from 'react-redux';
 import { incrementMistakes } from '@/store/slices/mistakesSlice';
 import { updateGameBoard } from '@/store/slices/sudokuSlice';
+import CandidateInput from './CandidateInput';
 
 interface NumericInputProps {
     value: string;
@@ -19,76 +20,98 @@ const CustomInput = (props: NumericInputProps) => {
   const { value, onChange, correctvalue, index } = props;
   const inputRef = useRef<InputRef>(null);
   const [background, setBackground] = useState('inherit')
+  const [selectedCandidates, setSelectedCandidates] = useState<string[]>([]);
+  const [isClick, SetIsClick] = useState(false)
   const dispatch = useDispatch()
     
   const handleMistake = () => {
     dispatch(incrementMistakes());
-  };
-    
+  } 
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
     const inputValue = e.target.value.slice(-1); 
-  
-
     const reg = /^\d$/;
+
     if (reg.test(inputValue) || inputValue === '') {
       onChange(inputValue);
       const isCorrect = inputValue === correctvalue;
     
-     
       if (!isCorrect && inputValue !== '') {
         setBackground('#D32F2F')
         handleMistake()
       } else {
         setBackground('inherit');
       }
+      inputRef.current?.input?.style.setProperty('cursor', 'default');
 
       dispatch(updateGameBoard({ index: index, value: inputValue }))
-
     }
-  };
+  }
 
   const handleChangeActive = () =>{
     setBackground('#E1AFD1')
+    SetIsClick(true)
     if (inputRef.current) {
       inputRef.current.focus(); 
       inputRef.current.select()
     }
-    
+    inputRef.current?.input?.style.setProperty('cursor', 'pointer');
   }
 
   
-  
   const handleBlur = () => {
+    
+
     let valueTemp = value;
     if (value.charAt(value.length - 1) === '.' || value === '-') {
       valueTemp = value.slice(0, -1);
     }
     onChange(valueTemp.replace(/0*(\d+)/, '$1'));
-    
-    if (background !== '#D32F2F') setBackground('inherit');
 
-    
-  };
+    if (background !== '#D32F2F') setBackground('inherit');
+    // SetIsClick(false)    
+  }
+
+ 
+
+  const handleCandidateSelect = (selectedValue: string) => {
+    setSelectedCandidates((prev) =>
+      prev.includes(selectedValue)
+        ? prev.filter((val) => val !== selectedValue)
+        : [...prev, selectedValue]
+    );
+  }
 
     return (
        
-      <Input
+      <div className={`${style.wrapper} ${isClick?style.reWrapper:''}`}
+        onClick={handleChangeActive}
+        onBlur={handleBlur}
+      >
+        {value === "" && isClick && (
+        
+        <CandidateInput
+          selectedValues={selectedCandidates}
+          onSelect={handleCandidateSelect}
+        />
+         
+      )}
+        <Input
         {...props}
         ref={inputRef} 
         onChange={handleChange}
-        onBlur={handleBlur}
         maxLength={1}
-        onClick={handleChangeActive}
         style={{backgroundColor:background, border:'0',textAlign:'center', height:'inherit',
            borderRadius:'0',caretColor: 'transparent', color:'#7469B6', fontWeight:'400', fontSize:'32px',
-           cursor:'pointer'
+           cursor:'default'
         }}
        className={style.inputStyle}
       />
-       
+      
+      </div>
     );
+      
 };
 
 export default CustomInput;
